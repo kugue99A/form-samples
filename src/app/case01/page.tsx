1a82dcfd48c341e0bc4cd04c666cb8df2791edea
kugue99A <hisabisa99yohey@gmail.com>
2024-10-20 23:17:51 +0900

    wip

--- src/app/case01/page.tsx
+++ src/app/case01/page.tsx
@@ -1,12 +1,57 @@
+'use client'
+
 import { Input } from '@/components'
+import { useForm, valiForm } from '@modular-forms/react'
+import * as v from 'valibot'
 import s from './style.module.css'
 
+type LoginForm = {
+	email: string
+	password: string
+}
+
+const LoginSchema: v.GenericSchema<LoginForm> = v.object({
+	email: v.pipe(
+		v.string(),
+		v.nonEmpty('Please enter your email.'),
+		v.email('The email address is badly formatted.'),
+	),
+	password: v.pipe(
+		v.string(),
+		v.nonEmpty('Please enter your password.'),
+		v.minLength(8, 'You password must have 8 characters or more.'),
+	),
+})
+
 export default function Case() {
+	const [_loginForm, { Form, Field }] = useForm<LoginForm>({
+		validate: valiForm(LoginSchema),
+	})
 	return (
 		<div className={s.container}>
 			<section className={s.root}>
 				<h1>case01</h1>
-				<Input />
+				<Input label="Name" name="name" />
+
+				<Form>
+					<Field name="email">
+						{(field, props) => (
+							<>
+								<input {...props} type="email" required />
+								{field.error && <div>{field.error.value}</div>}
+							</>
+						)}
+					</Field>
+					<Field name="password">
+						{(field, props) => (
+							<>
+								<input {...props} type="password" required />
+								{field.error && <div>{field.error.value}</div>}
+							</>
+						)}
+					</Field>
+					<button type="submit">Login</button>
+				</Form>
 			</section>
 		</div>
 	)
--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,5 +1,44 @@
+import type { ChangeEventHandler, FocusEventHandler } from 'react'
 import s from './style.module.css'
 
-export const Input = (): JSX.Element => {
-	return <input className={s.root} />
+type InputProps = {
+	name: string
+	type?: 'text' | 'email' | 'tel' | 'password' | 'url' | 'date'
+	label?: string
+	placeholder?: string
+	value?: string
+	error?: string
+	required?: boolean
+	onChange?: ChangeEventHandler<HTMLInputElement>
+	onBlur?: FocusEventHandler<HTMLInputElement>
+}
+
+export const Input = ({
+	name,
+	value,
+	label,
+	error,
+	placeholder,
+	type = 'text',
+	required,
+	...props
+}: InputProps): JSX.Element => {
+	return (
+		<div>
+			{label && (
+				<label htmlFor={name}>
+					{label} {required && <span>*</span>}
+				</label>
+			)}
+			<input
+				{...props}
+				className={s.root}
+				id={name}
+				value={value}
+				aria-invalid={!!error}
+				aria-errormessage={`${name}-error`}
+			/>
+			{error && <div id={`${name}-error`}>{error}</div>}
+		</div>
+	)
 }
